47fe143e17c26d1897b6a47bdc9166d42ebbd569
rpraneeth5225 <praneethregonda28@gmail.com>
2024-11-03 02:07:40 -0500

    added images to skills.js

---
+++ src/data/skills.js
@@ -0,0 +1,77 @@
+import html from '../images/skills/html.svg';
+import css from '../images/skills/css.svg';
+import javascript from '../images/skills/javascript.svg';
+import python from '../images/skills/python.svg';
+import react from '../images/skills/react.png';
+import npm from '../images/skills/npm.svg';
+import mongodb from '../images/skills/mongodb.png';
+import angular from '../images/skills/angular.png';
+import tailwind from '../images/skills/tailwind.svg';
+import git from '../images/skills/git.svg';
+import figma from '../images/skills/figma.svg';
+import typescript from '../images/skills/typescript.png';
+
+
+export const skills = [
+  {
+    id: 1,
+    title: 'HTML',
+    icon: html,
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    icon: css,
+  },
+  {
+    id: 3,
+    title: 'Javascript',
+    icon: javascript,
+  },
+  {
+    id: 4,
+    title: 'ReactJs',
+    icon: react,
+  },
+  {
+    id: 5,
+    title: 'npm',
+    icon: npm,
+    
+  },
+  {
+    id: 6,
+    title: 'Angular',
+    icon: angular,
+  },
+  {
+    id: 7,
+    title: 'MongoDB',
+    icon: mongodb,
+  },
+  {
+    id: 8,
+    title: 'Git & GitHub',
+    icon: git,
+  },
+  {
+    id: 9,
+    title: 'Tailwind',
+    icon: tailwind,
+  },
+  {
+    id: 10,
+    title: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    id: 11,
+    title: 'Figma',
+    icon: figma,
+  },
+  {
+    id: 12,
+    title: 'Python',
+    icon: python,
+  },
+];
